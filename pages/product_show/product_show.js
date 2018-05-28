60adf1b09e06c1d1cebf958bdf943cf94b6a85cf
Your Name <you@example.com>
2018-05-28 17:42:10 +0800

    'product_show'

--- pages/product_show/product_show.js
+++ pages/product_show/product_show.js
@@ -6,8 +6,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-    product_data:null,
-    product_id:null,
+    visibleTabKey: 'desc',
+    sku_num: 1,
+    product_data: null,
+    product_id: null,
+  },
+
+  setSkuNum: function (e) {
+    this.setData({
+      sku_num: Number(e.detail.value)
+    })
+  },
+  changeVisibleKey: function (e) {
+    this.setData({
+      visibleTabKey: e.currentTarget.dataset.key
+    })
+  },
+  changeProperty: function (e) {
+    this.setData({
+      product_id: e.currentTarget.dataset.oid
+    })
+    this.fetchData();
+  },
+  minusSkuNum: function () {
+    if (this.data.sku_num > 1) {
+      this.setData({
+        sku_num: this.data.sku_num - 1
+      })
+    }
+  },
+  plusSkuNum: function () {
+
+    this.setData({
+      sku_num: this.data.sku_num + 1
+    })
+
   },
 
   /**
@@ -19,20 +52,20 @@ Page({
     })
   },
 
-  fetchData:function(){
+  fetchData: function () {
     util.showLoading();
     wx.request({
-      url:util.url+'catalog/product/index',
-      data:{
-        product_id:this.data.product_id
+      url: util.url + 'catalog/product/index',
+      data: {
+        product_id: this.data.product_id
       },
-      success:res=>{
+      success: res => {
         wx.hideLoading();
         this.setData({
-          product_data:res.data.data.product
+          product_data: res.data.data.product
         })
       },
-      fail:()=>util.fail()
+      fail: () => util.fail()
     })
   },
 
@@ -40,14 +73,14 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    if(!this.data.product_data){
+    if (!this.data.product_data) {
       this.fetchData()
     }
   },
@@ -56,34 +89,34 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
